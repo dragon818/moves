d4972c9040a0553aa4e014d0580cd0793c015ee2
dragon818 <dragon.yao1219@gmail.com>
2019-12-17 11:18:39 -0600

    change the XHR to fetch API

--- main.js
+++ main.js
@@ -7,41 +7,35 @@ const getValue = function () {
   if (div) {
     div.innerHTML = '';
   }
-
-
-  let xhttp = new XMLHttpRequest();
-  let obj;
-  xhttp.onload = function() {
-    obj = JSON.parse(this.responseText)["Search"];
-    console.log(obj);
-    for(let i = 0; i < obj.length; i++) {
-      div.innerHTML += `<div class = "moveinfo" id = ${obj[i].imdbID}><img src="${obj[i].Poster}"><h3>---${obj[i].Type}---${obj[i].Title}</h3></div>`;
-    }   
-  };
-
-  xhttp.open("GET", `http://www.omdbapi.com/?s=${value}&apikey=6a0a2e62`);
-  xhttp.send();
+  fetch(`http://www.omdbapi.com/?s=${value}&apikey=6a0a2e62`)
+  .then(res => res.json())
+  .then(json => {
+    console.log(json);
+    json.Search.forEach((ele) => {
+      div.innerHTML += `<div class = "moveinfo" id = ${ele.imdbID}><img src="${ele.Poster}"><h3>---${ele.Type}---${ele.Title}</h3></div>`;
+    })
+  });
 }
 
 div.addEventListener("click", (e) => {
-  const de = document.querySelector(".detials");
+  const de = document.querySelector(".details");
+
   if (de) {
     de.outerHTML='';
   }
+
   if (e.target.parentElement.classList.contains("moveinfo")) {
-    let details = new XMLHttpRequest();
-    let obj1;
-    details.onload = function() {
-      obj1 = JSON.parse(this.response);
-      console.log(e.target.parentElement.id);
-      let ratingArray = obj1["Ratings"];
+    console.log(e.target.parentElement.id);
+    fetch(`http://www.omdbapi.com/?i=${e.target.parentElement.id}&apikey=6a0a2e62`)
+    .then(res => res.json())
+    .then(json => {
+      console.log(json);
+      let ratingArray = json.Ratings;
       let str = "";
       ratingArray.forEach(ele => { 
-        str += `<p>Source :${ele["Source"]} Value: ${ele["Value"]}</p>`
+        str += `<p>Source :${ele.Source} Value: ${ele.Value}</p>`
       });
-      e.target.parentElement.insertAdjacentHTML('beforeend', `<div class = "detials"><p>${obj1["Actors"]}</p><p>${obj1["Plot"]}</p><p>${obj1["Metascore"]}</p>${str}<p>${obj1["Released"]}</p></div>`);
-    };
-    details.open("GET", `http://www.omdbapi.com/?i=${e.target.parentElement.id}&apikey=6a0a2e62`);
-    details.send();
+      e.target.parentElement.insertAdjacentHTML('beforeend', `<div class = "details"><p>${json.Actors}</p><p>${json.Plot}</p><p>${json.Metascore}</p>${str}<p>${json.Released}</p></div>`);
+    });
   }
 })
